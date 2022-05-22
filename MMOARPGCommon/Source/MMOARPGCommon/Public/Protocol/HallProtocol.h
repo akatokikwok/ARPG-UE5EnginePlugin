1db0445304891a02d639ce3b4802856ee8eb350c
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-05-22 18:13:05 +0800

    [feat]: Validate the name, name check, and create the server framework for the role.

--- MMOARPGCommon/Source/MMOARPGCommon/Public/Protocol/HallProtocol.h
+++ MMOARPGCommon/Source/MMOARPGCommon/Public/Protocol/HallProtocol.h
@@ -6,7 +6,7 @@
 
 DEFINITION_SIMPLE_PROTOCOLS(CharacterAppearanceRequests, 4)
 DEFINITION_SIMPLE_PROTOCOLS(CharacterAppearanceResponses, 5)
-// DEFINITION_SIMPLE_PROTOCOLS(CheckCharacterNameRequests, 6)
-// DEFINITION_SIMPLE_PROTOCOLS(CheckCharacterNameResponses, 7)
-// DEFINITION_SIMPLE_PROTOCOLS(CreateCharacterRequests, 8)
-// DEFINITION_SIMPLE_PROTOCOLS(CreateCharacterResponses, 9)
+DEFINITION_SIMPLE_PROTOCOLS(CheckCharacterNameRequests, 6)
+DEFINITION_SIMPLE_PROTOCOLS(CheckCharacterNameResponses, 7)
+DEFINITION_SIMPLE_PROTOCOLS(CreateCharacterRequests, 8)
+DEFINITION_SIMPLE_PROTOCOLS(CreateCharacterResponses, 9)
