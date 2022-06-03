64c6a43c3234b1d2a2125ee96f900a4622c9567f
DESKTOP-6OVVB6Q\Askyr1m <akatokikwok@outlook.com>
2022-06-04 00:59:57 +0800

    [test]: 增加调试宏,用于测试网关机器人登录到DS后刷新人物样貌.

---
+++ MMOARPGCommon/Source/MMOARPGCommon/Public/Protocol/GameProtocol.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "SimpleProtocolsDefinition.h"
+
+DEFINITION_SIMPLE_PROTOCOLS(UpdateLoginCharacterInfoRequests, 18)
+DEFINITION_SIMPLE_PROTOCOLS(UpdateLoginCharacterInfoResponses, 19)
